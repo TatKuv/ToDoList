509ed70f60bcaba85b1af3249a416bedbd44d99b
tat <ttatiana.s.mails@gmail.com>
2022-09-21 00:10:49 +0300

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Tatiana Kuvarzina on 21.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
